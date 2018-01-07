e7700e77544c86d4cd73a2215cedd5f311829445
senvid <ssww1029@gmail.com>
2018-01-07 15:16:27 +0800

    'autoreload'

--- manage.py
+++ manage.py
@@ -15,10 +15,12 @@ from datetime import datetime
 
 
 if __name__ == "__main__":
+    # python manage.py --port=8000
     tornado.options.parse_command_line()
     app.listen(config.options.port)
     loop = tornado.ioloop.IOLoop.instance()
     tornado.autoreload.add_reload_hook(config.pool.closeAll)
+    # 新版本已自动开启
     tornado.autoreload.start(loop, 1000)
     start_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     logging.info('************  server start at %s ************' % start_time)
